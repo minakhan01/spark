bb5dcf26bc9728a03f07c5b08283179214ad80df
minakhan01 <minakhan01@gmail.com>
2016-04-24 13:36:17 -0700

    personal information fragment setup

--- app/src/main/java/com/spark/spark/AccountInformationFragment.java
+++ app/src/main/java/com/spark/spark/AccountInformationFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by khan32m on 4/23/16.
@@ -15,6 +17,22 @@ public class AccountInformationFragment extends Fragment {
                            Bundle savedInstanceState) {
     View view = null;
     view = inflater.inflate(R.layout.fragment_account_information, container, false);
+
+    Spinner genderSpinner = (Spinner) view.findViewById(R.id.gender_spinner);
+    ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.gender, android.R.layout.simple_spinner_item);
+    genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    genderSpinner.setAdapter(genderAdapter);
+
+    Spinner countryOfResidenceSpinner = (Spinner) view.findViewById(R.id.country_of_residence_input);
+    ArrayAdapter<CharSequence> countryOfResidenceAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.country_of_residence, android.R.layout.simple_spinner_item);
+    genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    countryOfResidenceSpinner.setAdapter(countryOfResidenceAdapter);
+
+    Spinner palestinianOrSyrianSpinner = (Spinner) view.findViewById(R.id.palestinian_or_syrian_input);
+    ArrayAdapter<CharSequence> palestinianOrSyrianAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.yes_no, android.R.layout.simple_spinner_item);
+    genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    palestinianOrSyrianSpinner.setAdapter(palestinianOrSyrianAdapter);
+
     return view;
   }
 }

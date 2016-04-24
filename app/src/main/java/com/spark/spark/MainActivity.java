fffa241c2d5301514dd22ea09afd2dfc3816c5f6
minakhan01 <minakhan01@gmail.com>
2016-04-23 21:42:13 -0700

    add fragments for application sections

--- app/src/main/java/com/spark/spark/MainActivity.java
+++ app/src/main/java/com/spark/spark/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -42,6 +44,7 @@ public class MainActivity extends AppCompatActivity
     navigationView.setNavigationItemSelectedListener(this);
   }
 
+  // todo: remove this
   public void startCameraACtivity() {
     Intent cameraActivityIntent = new Intent(this, CameraActivity.class);
     startActivity(cameraActivityIntent);
@@ -84,23 +87,48 @@ public class MainActivity extends AppCompatActivity
   public boolean onNavigationItemSelected(MenuItem item) {
     // Handle navigation view item clicks here.
     int id = item.getItemId();
-
-    if (id == R.id.nav_camera) {
-      // Handle the camera action
-    } else if (id == R.id.nav_gallery) {
-
-    } else if (id == R.id.nav_slideshow) {
-
-    } else if (id == R.id.nav_manage) {
-
-    } else if (id == R.id.nav_share) {
-
-    } else if (id == R.id.nav_send) {
-
-    }
+    displayView(id);
 
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     drawer.closeDrawer(GravityCompat.START);
     return true;
+  }
+
+  public void displayView(int id) {
+
+    Fragment fragment = null;
+    String title = getString(R.string.app_name);
+
+    if (id == R.id.nav_account_info) {
+      fragment = new AccountInformationFragment();
+      title = getResources().getString(R.string.account_info);
+    } else if (id == R.id.nav_personal_info) {
+      fragment = new PersonalInformationFragment();
+      title = getResources().getString(R.string.personal_info);
+    } else if (id == R.id.nav_past_experience) {
+      fragment = new PreviousExperienceFragment();
+      title = getResources().getString(R.string.previous_study);
+    } else if (id == R.id.nav_field_studies) {
+      fragment = new FieldStudiesFragment();
+      title = getResources().getString(R.string.field_studies);
+    } else if (id == R.id.nav_stimulus) {
+      fragment = new StimulusFragment();
+      title = getResources().getString(R.string.stimulus);
+    } else if (id == R.id.nav_send) {
+      fragment = new RecordAndSendFragment();
+      title = getResources().getString(R.string.record_send);
+    }
+
+    if (fragment != null) {
+      FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+      ft.replace(R.id.content_frame, fragment);
+      ft.commit();
+    }
+
+    // set the toolbar title
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().setTitle(title);
+    }
+
   }
 }

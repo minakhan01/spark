533620dc8968fc5b2d8f1a293e8b66ba2b43d1e8
minakhan01 <minakhan01@gmail.com>
2016-04-24 13:00:44 -0700

    setting up form fragments

--- app/src/main/java/com/spark/spark/AccountInformationFragment.java
+++ app/src/main/java/com/spark/spark/AccountInformationFragment.java
@@ -1,9 +1,20 @@
 package com.spark.spark;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by khan32m on 4/23/16.
  */
 public class AccountInformationFragment extends Fragment {
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View view = null;
+    view = inflater.inflate(R.layout.fragment_account_information, container, false);
+    return view;
+  }
 }
